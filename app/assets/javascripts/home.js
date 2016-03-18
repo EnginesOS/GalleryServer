c51e897c313e796bb9fc9850266255847e9881e2
Lachlan Douglas <lachlan@engines.org>
2016-03-18 16:14:50 +1100

    add Gemfile.lock to git.ignore and remove tag cloud js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,13 +2,6 @@ $(document).ready(function(){
 
     if (document.getElementsByClassName("home_page").length) {
 
-      $("html").addClass("home_page_background_cover");
-
-      $.get("tags/tag_cloud", function(data){
-            $("#tag_cloud_holder").html(data);
-            bind_trigger_response_modal_events();
-      });
-
       $('#home-page-video-modal').on('shown.bs.modal', function() {
         $('#home-page-video-modal video')[0].play();
       });
